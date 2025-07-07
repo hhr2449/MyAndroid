2954f88f7fa54fb2dea0e2e8cb11533fbc9229d2
hhr <2627367147@qq.com>
2025-07-07 23:55:00 +0800

    新闻app正式项目，已经完成了侧滑抽屉，分类栏，读取新闻的FetchNews类

---
+++ test5/app/src/main/java/com/java/huhaoran/MainActivity.java
@@ -0,0 +1,118 @@
+package com.java.huhaoran;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.java.huhaoran.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tablayout;
+    private ViewPager2 viewpager;
+    //这是新闻主题的数组，用来创建TabLayout的标签
+    private String[] titles = {"全部","娱乐", "军事", "教育", "文化", "健康", "财经", "体育", "汽车", "科技", "社会"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView menu = findViewById(R.id.menu);
+        menu.setOnClickListener(v -> {
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            drawer.openDrawer(GravityCompat.START);
+        });
+
+//----------------------实现抽屉栏的返回键-------------------------//
+        //注意这个图标是属于 NavigationView中的头部栏的，所以不能直接获取
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0); // 获取第一个头部视图
+        ImageView backbutton = headerView.findViewById(R.id.backbutton);
+        //设置点击滑动抽屉的返回键
+        backbutton.setOnClickListener(v -> {
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            //关闭抽屉
+            drawer.closeDrawer(GravityCompat.START);
+        });
+//----------------------实现抽屉栏的返回键-------------------------//
+
+
+//----------------------实现主题栏-----------------------------------------------
+        //初始化tablayout和viewpager，用于实现滑动新闻分类
+        tablayout = findViewById(R.id.tablayout);
+        viewpager = findViewById(R.id.viewpager);
+
+        //viewpager需要设置Adapter,这里使用FragmentStateAdapter
+        //ViewPager是容器，负责显示页面和处理手部滑动
+        //FragmentStateAdapter是Fragment的适配器，负责向ViewPager提供Fragment，并且管理Fragment的生命周期
+        //Fragment是每个页面具体显示的内容
+        viewpager.setAdapter(new FragmentStateAdapter(this) {
+            //这里创建了一个匿名的FragmentStateAdapter对象
+            @NonNull
+            @Override
+            //这个方法的返回值是一个Fragment对象，当ViewPager需要显示一个页面的时候就会调用
+            //这个方法，根据位置返回一个Fragment对象
+            public Fragment createFragment(int position) {
+                //使用newInstance()方法创建一个Fragment对象，传入的主题参数使用titles[position]获取
+                return TabNewsFragment.newInstance(titles[position]);
+            }
+
+            @Override
+            //告诉ViewPager一共有多少个页面
+            public int getItemCount() {
+                //总的页面数就是标题的个数
+                return titles.length;
+            }
+        });
+
+        //tablayout点击事件
+        tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //设置viewPager选中当前页
+                //tab.getPosition()获取当前选中的tab的索引
+                viewpager.setCurrentItem(tab.getPosition(),true);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        //viewPager和tab_layout关联在一起
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tablayout, viewpager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(titles[position]);
+            }
+        });
+
+        //启用绑定
+        tabLayoutMediator.attach();
+//----------------------实现主题栏-----------------------------------------------
+
+
+
+    }
+
+
+}
---
+++ test5/app/src/main/java/com/java/huhaoran/TabNewsFragment.java
@@ -0,0 +1,63 @@
+package com.java.huhaoran;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TabNewsFragment extends Fragment {
+
+    //因为Bundle储存数据是使用键值对的，所以需要设置常量参数作为key，实际想要传递的数据作为 value
+    //我们需要一个参数就是页面的主题
+    private static final String ARG_PARAM = "title";
+
+    // TODO: Rename and change types of parameters
+    private String title;
+
+    public TabNewsFragment() {
+
+    }
+
+    public static TabNewsFragment newInstance(String title) {
+        //newInstance()方法返回一个TabNewsFragment对象
+        TabNewsFragment fragment = new TabNewsFragment();
+        //Bundle类是Android中用于在Activity之间传递数据的类
+        /*
+        ①putXxx(String key,Xxx value):Xxx表示一系列的数据类型，
+        比如String,int,float,Parcelable，Serializable等类型，
+        以键-值对形式保存数据。
+        ②getXxx(String key):根据key值获取Bundle中的数据。
+        */
+        //创建Bundle对象，将参数传入并且使用setArguments()方法将Bundle对象与Fragment对象关联起来
+        //Bundle对象的作用在于，当旋转屏幕等情况下，Fragment需要销毁后再重新创建，此时会保存Bundle对象用于重建
+        //如果直接赋值给成员变量，那么销毁的时候数据就会丢失
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM, title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    //生命周期开始，注意onCreate()方法是系统自动调用的，
+    // newInstance()方法只是预先创建对象并且传入参数，
+    // 等到需要页面的时候其返回的TabNewsFragment对象会自动调用这个方法创建液面1
+    public void onCreate(Bundle savedInstanceState) {
+        //调用父类的onCreate()方法，初始化页面
+        super.onCreate(savedInstanceState);
+        //将之前的参数传入
+        if (getArguments() != null) {
+            title = getArguments().getString(ARG_PARAM);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_news, container, false);
+    }
+}
